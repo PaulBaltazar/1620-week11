4039a51b8bd1031de2e40071545cfa20e432ef66
Paul <paulpetergb@hotmail.com>
2022-11-15 15:35:02 -0800

    innerteext vs innerHTML vs text content

--- scripts/script.js
+++ scripts/script.js
@@ -13,6 +13,7 @@
 
 // greet("Paul")
 
+
 const greet = function (user, time = "day")
 {
     console.log(`Good ${time} ${user}`)
@@ -22,14 +23,35 @@ greet("Paul");
 greet("Paul", "Night", "Morning")
 // greet("Paul", "Night"); order matters
 
+
+// return statement activity
+// let r = prompt("Enter a radius.");
+// console.log(r)
+
+// function circleArea (radius)
+// {
+//     return Math.PI * radius * radius;
+// }
+
+// let area = circleArea(r).toFixed(2); 
+// // Math.round
+// alert(`The area of a circle with a radius ${r} is ${area}`)
+
+//document.getElementId() - text content
 let r = prompt("Enter a radius.");
 console.log(r)
 
 function circleArea (radius)
 {
-    return Math.PI * radius * radius;
+    const radiusP = document.querySelector("#radius");
+    const resultP = document.querySelector("#result");
+    radius.textContent =radius;
+    let calculatedArea = Math.PI * radius * radius;
+    resultP.textContent = calculatedArea
+    return calculatedArea;
 }
 
 let area = circleArea(r).toFixed(2); 
 
 alert(`The area of a circle with a radius ${r} is ${area}`)
+
